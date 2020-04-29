24b4fe140223f2fe5164b18e0eb52a2bef4b8121
root <root@PC.localdomain>
2020-04-29 17:16:40 -0300

    Definindo as bases do projeto

---
+++ database/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('guiapress', 'root', '3282',{
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,32 @@
+const Express = require('express');
+const app = Express();
+const bodyParser= require('body-parser');
+const connection = require('./database/database');
+
+//View Engine
+app.set('view engine', 'ejs');
+
+//Static
+app.use(Express.static('public'));
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Database
+connection
+    .authenticate()
+    .then(()=>{
+        console.log('Conectado com o banco de dados')
+    })
+    .catch(msgErr =>{
+        console.log('Ocorreu um erro na conexão com o banco \n'+msgErr);
+    });
+
+app.get('/',(req, res)=>{
+    res.render('index')
+});
+
+app.listen(8080, ()=>{
+    console.log("O servidor esta rodando")
+})
