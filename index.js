8a0fdbea90a126069baea37a7081d15d2b125086
root <root@PC.localdomain>
2020-05-01 12:19:15 -0300

    Corrigido padroes de codigos errados

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const Express = require('express');
+const Express = require("express");
 const app = Express();
-const bodyParser= require('body-parser');
-const connection = require('./database/database');
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
 //View Engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //Static
-app.use(Express.static('public'));
+app.use(Express.static("public"));
 
 //Body Parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Database
 connection
-    .authenticate()
-    .then(()=>{
-        console.log('Conectado com o banco de dados')
-    })
-    .catch(msgErr =>{
-        console.log('Ocorreu um erro na conexão com o banco \n'+msgErr);
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Conectado com o banco de dados");
+  })
+  .catch((msgErr) => {
+    console.log("Ocorreu um erro na conexão com o banco \n" + msgErr);
+  });
 
-app.get('/',(req, res)=>{
-    res.render('index')
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.listen(8080, ()=>{
-    console.log("O servidor esta rodando")
-})
+app.listen(8080, () => {
+  console.log("O servidor esta rodando");
+});
