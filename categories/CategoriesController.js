13423e7859e691c86604e38c667405001cd390cf
root <root@PC.localdomain>
2020-05-02 13:20:29 -0300

    Criado a rota que salva a categoria no bd

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,25 @@
 const Express = require("Express");
 const router = Express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
+});
+
+router.get("/categories/save", (req, res) => {
+  const { title } = req.body;
+
+  if (title != undefined) {
+    Category.create({
+      title,
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect("/");
+    });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
 });
 
 module.exports = router;
