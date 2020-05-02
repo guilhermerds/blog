414860599c0f363f785e31732d27efe5db4f5e41
root <root@PC.localdomain>
2020-05-01 21:50:51 -0300

    Criado o model de category

---
+++ categories/Category.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const Category = connection.define("category", {
+  title: { type: Sequelize.STRING, allowNull: false },
+  slug: { type: Sequelize.STRING, allowNull: false },
+});
+
+/*SLUG é a versão do titulo sem caracter especial e sem espaço,
+EX title: react native, slug: react-native*/
+
+module.exports = Category;
