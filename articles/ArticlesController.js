aeaf3fd4c8148d291146ccc9e344fa3dc959f36b
root <root@PC.localdomain>
2020-05-04 14:41:16 -0300

    Criado a rota de exclusao de artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -30,4 +30,15 @@ router.post("/articles/save", (req, res) => {
   });
 });
 
+router.post("/articles/delete", (req, res) => {
+  const { id } = req.body;
+  if (id != undefined && !isNaN(id)) {
+    Article.destroy({ where: { id } }).then(() => {
+      res.redirect("/admin/articles");
+    });
+  } else {
+    res.redirect("/admin/articles");
+  }
+});
+
 module.exports = router;
