16fd46e73c2fbcf2df076693f7c142554ef1c278
root <root@PC.localdomain>
2020-05-04 14:05:36 -0300

    Corrigido o time zone do sequelize

--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("guiapress", "root", "3282", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
