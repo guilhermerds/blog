06dde7239103b5d4d7720e01773c94b75118228d
root <root@PC.localdomain>
2020-05-28 15:02:11 -0300

    Criado a schema da tabela users

---
+++ users/User.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+
+const User = connection.define("Users", {
+  email: { type: Sequelize.STRING, allowNull: false },
+  password: { type: Sequelize.STRING, allowNull: false },
+});
+
+module.exports = User;
