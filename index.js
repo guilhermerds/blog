253ce3f2a79777e671d74279f984c66d96c7091b
root <root@PC.localdomain>
2020-05-28 15:04:19 -0300

    Adicionado as rotas de usuarios

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const connection = require("./database/database");
 
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+const userController = require("./users/UserController");
 
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
@@ -32,6 +33,7 @@ connection
 //Categories, a barra na frente indica o prefixo
 app.use("/", categoriesController);
 app.use("/", articlesController);
+app.use("/", userController);
 
 app.get("/", (req, res) => {
   Article.findAll({ order: [["id", "DESC"]], limit: 4 }).then((articles) => {
